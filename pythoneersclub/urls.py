454be3fd6db58435aa5300c1afac3cc130594f79
Jason Blum <jason_blum@saa.senate.gov>
2020-05-02 08:16:16 -0400

    typo

--- pythoneersclub/urls.py
+++ pythoneersclub/urls.py
@@ -5,6 +5,6 @@ from django.views.generic.base import RedirectView, TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html')),
     path('zulu/', admin.site.urls),
-    path('trinket/', RedirectView.as_view(url='https://trinket.io/courses/join/jXnYU8')),
+    path('trinket/', RedirectView.as_view(url='https://trinket.io/courses/join/JXnYU8')),
     path('chat/', RedirectView.as_view(url='http://discord.gg/2USePEX')),
 ]
