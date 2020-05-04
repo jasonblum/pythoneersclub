27a3c25476c315de87ffb50bedbcd168f282604d
Jason Blum <jason_blum@saa.senate.gov>
2020-05-04 07:10:17 -0400

    home page

--- pythoneersclub/settings.py
+++ pythoneersclub/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'shared.apps.SharedConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shared/static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder", 
+]
--- pythoneersclub/urls.py
+++ pythoneersclub/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import RedirectView, TemplateView
 
+from .views import home
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html')),
+	path('', home, name='home'),
     path('zulu/', admin.site.urls),
     path('trinket/', RedirectView.as_view(url='https://trinket.io/courses/join/JXnYU8')),
     path('chat/', RedirectView.as_view(url='http://discord.gg/2USePEX')),
 ]
+
+
+
+
