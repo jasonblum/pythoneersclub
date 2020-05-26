d5e6da31c5e38a36f3cc91b9c00db2de3815c7ff
Jason Blum <jasonblum@users.noreply.github.com>
2020-05-26 06:41:34 -0400

    Update urls.py

--- pythoneersclub/urls.py
+++ pythoneersclub/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     re_path(r'^(?i)cheatsheet/', RedirectView.as_view(url='https://learnxinyminutes.com/docs/python/')),
     re_path(r'^(?i)codeacademy/', RedirectView.as_view(url='https://www.codecademy.com/learn/learn-python-3')),
     re_path(r'^(?i)comics/', RedirectView.as_view(url='https://www.dropbox.com/sh/28ab0g5jags1xam/AAAYoFDBXk90V_-TSFjlb8O6a')),
-    re_path(r'^(?i)huskies/', RedirectView.as_view(url='https://drive.google.com/drive/folders/1zIXnPedsIrg0uOK9oPx2dGUjC_D5mp8b?usp=sharing')),
+    re_path(r'^(?i)join/', RedirectView.as_view(url='https://forms.gle/aQaa9qyFwxNSA7HB7')),
     re_path(r'^(?i)socratica/', RedirectView.as_view(url='https://www.youtube.com/playlist?list=PLi01XoE8jYohWFPpC17Z-wWhPOSuh8Er-')),
     re_path(r'^(?i)trinket/', RedirectView.as_view(url='https://trinket.io/courses/join/JXnYU8')),
 	
